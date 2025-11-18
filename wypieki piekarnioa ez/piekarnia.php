5e4b10e232bb7e7045340ddad25450b94318973a
victoria915 <vlidzbarska1@gmail.com>
2025-11-18 13:51:07 +0100

    Add files via upload

---
+++ wypieki piekarnioa ez/piekarnia.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>PIEKARNIA</title>
+</head>
+<body>
+    <header>
+        <img src="wypieki.jpg" class="tlo">
+        <h1>WITAMY</h2>
+        <h4>NA STRONIE PIEKARNI</hr4>
+        <p>Od 31 lat oferujemy najwyższej jakości pieczywo. Naturalnie świeże, naturalnie smaczne. Pieczemy wyłącznie wypieki na naturalnym zakwasie bez polepszaczy i zagęstników. Korzystamy wyłącznie z najlepszych ziaren pochodzących z ekologicznych upraw położonych w rejonach zgierskim i ozorkowskim.</p>
+    </header>
+    <main>
+        <h4>Wybierz rodzaj wypieków:</h4>
+
+        <table>
+            <tr>
+                <th>Rodzaj</th>
+                <th>Nazwa</th>
+                <th>Gramatura</th>
+                <th>Cena</th>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <p>Autor:Victoria L</p>
+        <p>Data:09.09.2025</p>
+    </footer>
+
+
+</body>
+</html>
